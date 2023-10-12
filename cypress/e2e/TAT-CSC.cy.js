dcce4e379903a075026a1d95c87fe96f3dd21833
Wrajt <kgrabarek59@gmail.com>
2023-10-12 14:23:14 +0200

    update 1.1

---
+++ cypress/e2e/TAT-CSC.cy.js
@@ -0,0 +1,6 @@
+describe('TAT Customer Service Center', () => {
+  it('checks the application title', () => {
+    cy.visit("../src/index.html")
+    cy.title().should('eq', 'TAT Customer Service Center')
+  })
+})
