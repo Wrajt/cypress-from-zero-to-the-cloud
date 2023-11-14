353c960e369a96e82d0d7dd4a7a8abfd0fef6034
Wrajt <kgrabarek59@gmail.com>
2023-11-14 14:37:14 +0100

    update 5.1. CI/CD

--- cypress/e2e/TAT-SCS-opening-tabs-in-new-window.cy.js
+++ cypress/e2e/TAT-SCS-opening-tabs-in-new-window.cy.js
@@ -2,7 +2,7 @@ describe('TAT Customer Service Center', () => {
     beforeEach(() => {
         cy.visit("../src/index.html")
     })
-    it('verifies that the privacy policy page opens in another tab without the need for a click', () =>{
+    it.only('verifies that the privacy policy page opens in another tab without the need for a click', () =>{
         cy.contains('a', 'Privacy Policy').should('have.attr', 'target', '_blank')
     })
     it('access the privacy policy page by removing the target, then clicking on the link', () => {
